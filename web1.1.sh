d8f047a9cda3f59c4b09255e7ed97d77f04e1bbb
Jeff Blubaugh <jblubaugh@Jeffs-MBP.attlocal.net>
2018-12-21 17:13:24 -0800

    edited script files

---
+++ web1.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /tmp/variables.sh
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install apache2 mysql-client php7.2 libapache2-mod-php7.2 php-mysql php-curl php-json php-cgi -y
+sudo sed -i 's/^KeepAlive .*/KeepAlive On/' /etc/apache2/apache2.conf
+sudo sed -i 's/^MaxKeepAliveRequests.*/MaxKeepAliveRequests 50/' /etc/apache2/apache2.conf
+sudo sed -i 's/^KeepAliveTimeout.*/KeepAliveTimeout 5/' /etc/apache2/apache2.conf
