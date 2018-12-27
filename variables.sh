aaf375429d628de950d0b04102205df430ebca63
Jeffery A Blubaugh <43864066+blubaughj@users.noreply.github.com>
2018-12-27 13:35:10 -0800

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -3,3 +3,4 @@ export DB_PASSWORD="${DB_PASSWORD:-password123}"
 export DB_USER="${DB_USER:-bluuser}"
 export DB_NAME="${DB_NAME:-bludb}"
 export SITE_NAME="${SITE_NAME:blupress}"
+export SITE_ROOT_DIR=/var/www/html/blupress.com
